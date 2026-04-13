c2eb3869cb359a6d944940189662af70c03fb507
Javier Ignacio Rivarola <156439571+javierrivarola1@users.noreply.github.com>
2026-04-13 20:22:00 -0300

    Add files via upload

---
+++ java_class1_03/src/java_class1_03/Java_class1_03.java
@@ -0,0 +1,25 @@
+
+package java_class1_03;
+
+
+public class Java_class1_03 {
+
+ 
+    public static void main(String[] args) {
+       
+        
+        int num1 = 35;
+        int num2 = 20;
+        int num3 = 0;
+        
+        num3=num1;//35
+        num1=num2;//20
+        num2=num3;//35
+        num3=0;
+        
+        System.out.println("Los resultados serian: " + num1 + " " + num2 + " " + num3);
+        
+        
+    }
+    
+}
