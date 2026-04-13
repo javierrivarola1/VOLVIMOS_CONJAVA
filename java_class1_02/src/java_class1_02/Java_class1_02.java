5f24366ba846630cf39e7957fce2552ab8f9ef04
Javier Ignacio Rivarola <156439571+javierrivarola1@users.noreply.github.com>
2026-04-12 23:07:31 -0300

    Add files via upload

---
+++ java_class1_02/src/java_class1_02/Java_class1_02.java
@@ -0,0 +1,18 @@
+
+package java_class1_02;
+
+
+public class Java_class1_02 {
+
+  
+    public static void main(String[] args) {
+        
+        int valorNormal = 123;
+        final double VALOR_CONSTANTE = 1.0;
+        final double RESULTADO;
+        RESULTADO = VALOR_CONSTANTE*2;
+        
+        System.out.println("Tenemos 2 resultados distintos " + valorNormal + " y " + RESULTADO);
+    }
+    
+}
