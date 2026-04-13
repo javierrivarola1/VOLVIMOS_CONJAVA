db9d2ec08e710a9e25a9d9f862bf58b2ce6c5c0e
Javier Ignacio Rivarola <156439571+javierrivarola1@users.noreply.github.com>
2026-04-12 22:39:27 -0300

    Variables_01

---
+++ java_class1/src/java_class1/Java_class1.java
@@ -0,0 +1,21 @@
+
+package java_class1;
+
+
+public class Java_class1 {
+
+    public static void main(String[] args) {
+        
+        //variables
+        
+        int num = 1;
+        double numeroconComa = 1.2;
+        char unaLetra = 'c';
+        boolean verdadero = true;
+        
+        System.out.println("Los resultados de las variables son: "
+                + " " + num + " " + numeroconComa + " " + unaLetra + " " + verdadero);
+       
+    }
+    
+}
