36044426d67cff15e6afafa0ddc8b79e72e3796c
Javier Ignacio Rivarola <156439571+javierrivarola1@users.noreply.github.com>
2026-05-02 13:50:14 -0300

    Add files via upload

---
+++ java_class_bucles_03/src/java_class_bucles_03/Java_class_bucles_03.java
@@ -0,0 +1,35 @@
+//Escribir un programa que mientras que el usuario ingrese un número entero entre 1 y 10 inclusive,
+//lleve la suma de los números ingresados. 
+//Finalmente, cuando sale del ciclo muestre por pantalla el resultado de la suma. ¿En qué casos termina?
+
+
+package java_class_bucles_03;
+
+
+public class Java_class_bucles_03 {
+
+
+    public static void main(String[] args) {
+      
+        int numeroIng = 0;
+        
+        int suma = 0;
+        
+        System.out.println("Por favor dame un numero entre 1 y 10 ");
+        numeroIng = Utils.leerInt();
+        
+     while((numeroIng >=1) && (numeroIng <=10)){
+         suma = numeroIng+numeroIng;
+     
+        System.out.println("Excelente, dame otro numero entre el 1 y el 10 ");
+        numeroIng = Utils.leerInt();
+        
+    }
+     
+        System.out.println("el resultado de la suma de los numeros ingresados es: " + suma);
+     
+    }
+    
+    
+    
+}
